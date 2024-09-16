7361e265dee90efb521337a05cc24f78edb68883
matthes <matthesboettcher03@gmail.com>
2024-09-16 22:57:44 +0200

    Animationen aktualisiert

---
+++ buttonListener.js
@@ -0,0 +1,29 @@
+document.getElementById("headerListButton").onclick = function() {
+    myFunction();
+}
+
+function myFunction() {
+    console.log("click")
+}
+
+document.getElementById("headerInstagramButton").onclick = function() {
+    myFunction();
+}
+
+document.getElementById("headerGithubButton").onclick = function() {
+    myFunction();
+}
+
+
+
+
+let box = document.querySelector(".box");
+let play = document.querySelector(".stopAndPlayButton");
+
+play.addEventListener('click', () => {
+    if (box.style.animationPlayState == 'running') {
+        box.style.animationPlayState = 'paused';
+    } else {
+        box.style.animationPlayState = 'running';
+    }
+});
