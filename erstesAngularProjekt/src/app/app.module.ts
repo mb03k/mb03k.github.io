24f5447ef6143eb34cdebf1fee7dba971335bc04
matthes <matthesboettcher03@gmail.com>
2024-09-25 21:35:49 +0200

    vorerst finale Version

--- erstesAngularProjekt/src/app/app.module.ts
+++ erstesAngularProjekt/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses/courses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule,
